b17ddc24dcd0c9f7bdfc3c1b879cb5f82c701852
Eisuke15 <hideyu41691@gmail.com>
2021-04-08 15:29:25 +0900

    broadcast

---
+++ scheduler.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, abort
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+def main():
+    pushText = TextSendMessage(text="体温を計測してね")
+    line_bot_api.broadcast(USER_ID, messages=pushText)
+
+if __name__ == "__main__":
+    main()
