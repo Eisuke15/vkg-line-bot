fe92eeaa8149c0bfb619d47d1fedb4ef9928ac4d
Eisuke15 <hideyu41691@gmail.com>
2021-04-28 16:54:53 +0900

    blueprint作成

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,31 +1,19 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, abort, request
-from linebot import LineBotApi, WebhookHandler
+from flask import Flask, request, abort
+
+from .linebot import bp, handler
+from .models import Base, engine
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import (JoinEvent, LeaveEvent, MessageEvent, TextMessage,
-                            TextSendMessage)
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from .models import Base, Group
-
+#環境変数ファイルから読み込み
 load_dotenv()
 
 app = Flask(__name__)
 
 #Flaskのconfig設定
 app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
-
-#Lineのアクセストークン、アクセスキー取得
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
-
-#DB用初期設定
-engine = create_engine(os.environ["DB_URL"], convert_unicode=True)
-Session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base.metadata.create_all(bind=engine)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -36,6 +24,9 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # テーブル作成
+    Base.metadata.create_all(bind=engine)
+
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -44,30 +35,4 @@ def callback():
 
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    if event.source.type == "user":
-        line_bot_api.reply_message(
-            event.reply_token, 
-            TextSendMessage(text=event.message.text))
-
-@handler.add(JoinEvent)
-def handle_join(event):
-    group_id = event.source.group_id
-
-    session = Session()
-    session.add(Group(group_id))
-    session.commit()
-    session.close()
-
-    # pushText = TextSendMessage(text="追加ありがとうございます。")
-    # line_bot_api.push_message(to=group_id,messages=pushText)
-
-@handler.add(LeaveEvent)
-def handle_leave(event):
-    group_id = event.source.group_id
-
-    session = Session()
-    session.query(Group).filter(Group.g_id == group_id).delete()
-    session.commit()
-    session.close()
+app.register_blueprint(bp)
--- bot/models.py
+++ bot/models.py
@@ -1,6 +1,16 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
+import os
 
+from dotenv import load_dotenv
+from sqlalchemy import Column, DateTime, Integer, String, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#環境変数ファイルから読み込み
+load_dotenv()
+
+#DB用初期設定
+engine = create_engine(os.environ["DB_URL"], convert_unicode=True)
+Session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
 
 class Group(Base):
